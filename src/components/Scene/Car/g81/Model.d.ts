83b3c5c659bef0dbdb28b517560f1d19f77520b7
seymzr <seymzr@gmail.com>
2024-06-24 10:22:12 +0400

    ts config fix

---
+++ src/components/Scene/Car/g81/Model.d.ts
@@ -0,0 +1,8 @@
+// src/components/Scene/Car/g81/Model.d.ts
+
+declare module '../../components/Scene/Car/g81/Model' {
+    import { ComponentType } from 'react';
+  
+    export const Bmw: ComponentType;
+  }
+  
